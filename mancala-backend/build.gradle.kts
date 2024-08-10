b58191faed5a62ad6f71e3caef44785b660a1760
lameeskamal <lkamal@attraqt.com>
2024-08-10 21:03:25 +0200

    * Add basic Springboot dependencies * Configure Kotlin to use Java 17 and have stricter nullability checks * configure test gradle tasks to execute JUnit 5 tests for better features

--- mancala-backend/build.gradle.kts
+++ mancala-backend/build.gradle.kts
@@ -1,21 +1,34 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    id("org.springframework.boot") version "3.0.0"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.0"
 }
 
-group = "org.example"
+group = "com.example"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        // Enforces strict nullability checks based on JSR 305 annotations
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
-kotlin {
-    jvmToolchain(17)
+
+// Configures test tasks to use JUnit platform for running JUnit 5 tests
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
